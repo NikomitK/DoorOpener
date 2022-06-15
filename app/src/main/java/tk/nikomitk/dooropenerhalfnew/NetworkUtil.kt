940635a174f975197b281d4c801d87350965abc7
Niko <nikobo2003@gmail.com>
2022-06-15 12:51:17 +0200

    removed unused code and imports

--- app/src/main/java/tk/nikomitk/dooropenerhalfnew/NetworkUtil.kt
+++ app/src/main/java/tk/nikomitk/dooropenerhalfnew/NetworkUtil.kt
@@ -5,11 +5,8 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
-import tk.nikomitk.dooropenerhalfnew.messagetypes.LoginMessage
-import tk.nikomitk.dooropenerhalfnew.messagetypes.Message
 import tk.nikomitk.dooropenerhalfnew.messagetypes.Response
 import java.io.BufferedReader
-import java.io.File
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.InetSocketAddress
@@ -18,8 +15,6 @@ import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
 
 object NetworkUtil {
-    // TODO unite the methods with message: String parameter
-    private val gson: Gson = Gson()
 
     suspend fun sendMessage(
         message: String,
@@ -27,8 +22,7 @@ object NetworkUtil {
     ): Response {
         val test: Deferred<Response> = coroutineScope {
             async(Dispatchers.IO) {
-                val response = sendTLsMessage(message, ipAddress)
-                return@async response ?: run {
+                return@async sendTLsMessage(message, ipAddress) ?: run {
                     val socket = Socket()
                     try {
                         socket.connect(InetSocketAddress(ipAddress, 5687), 1500)
@@ -49,40 +43,6 @@ object NetworkUtil {
         return test.await().takeUnless {
             it.text.isBlank()
         } ?: Response("Invalid response", "invalid response")
-    }
-
-    suspend fun sendLoginMessage(
-        ipAddress: String,
-        pin: Int,
-        storage: Storage,
-        newDevice: Boolean,
-        rememberPassword: Boolean,
-        storageFile: File,
-        tls: Boolean
-    ): Response {
-        val loginMessage = LoginMessage("login", pin, newDevice)
-        val test: Deferred<Response> = coroutineScope {
-            async(Dispatchers.IO) {
-                val response = sendTLsMessage(gson.toJson(loginMessage), ipAddress)
-                return@async response ?: run {
-                    val socket = Socket()
-                    try {
-                        socket.connect(InetSocketAddress(ipAddress, 5687), 1500)
-                        PrintWriter(socket.outputStream, true).println(gson.toJson(loginMessage))
-                        return@async Gson().fromJson(
-                            BufferedReader(InputStreamReader(socket.inputStream)).readLine(),
-                            Response::class.java
-                        )
-                    } catch (exception: Exception) {
-                        return@async Response("Timeout :c", "timeout")
-                    } catch (exception: java.lang.Exception) {
-                        exception.printStackTrace()
-                        return@async Response(exception.message!!, "Not sent")
-                    }
-                }
-            }
-        }
-        return test.await()
     }
 
     private fun sendTLsMessage(message: String, ipAddress: String): Response? {
